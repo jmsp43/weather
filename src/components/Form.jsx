b7d7a9670c4dee7503f9e9b7d10b665cc7f22d99
Jenna Perez <jmsp43@gmail.com>
2023-04-25 20:05:42 -0400

    fixed real feel update

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,11 +4,12 @@ import FetchWeather from "../services/FetchWeather";
 import Current from "./Current";
 
 export default function Form() {
-    let inputVal = useRef();
-    let unitChoice = useRef();
+  let inputVal = useRef();
+  let unitChoice = useRef();
   const [tempState, setTempState] = useState(null);
-    const [feelState, setFeelState] = useState(null);
-    const [baseUnit, setBaseUnit] = useState(null)
+  const [feelState, setFeelState] = useState(null);
+  const [baseUnit, setBaseUnit] = useState(null);
+  const [baseFeel, setBaseFeel] = useState(null);
   const [neighborhoodState, setNeighborhoodState] = useState("");
   const [descriptionState, setDescriptionState] = useState("");
 
@@ -27,8 +28,9 @@ export default function Form() {
     weather.then((weatherData) => {
       if (weatherData) {
         setTempState(KtoF(weatherData.main.temp));
-          setFeelState(KtoF(weatherData.main.feels_like));
-          setBaseUnit(KtoF(weatherData.main.temp))
+        setFeelState(KtoF(weatherData.main.feels_like));
+        setBaseUnit(KtoF(weatherData.main.temp));
+        setBaseFeel(KtoF(weatherData.main.feels_like));
         setNeighborhoodState(weatherData.name);
         setDescriptionState(weatherData.weather[0].description);
       } else {
@@ -37,46 +39,43 @@ export default function Form() {
     });
   }
 
-  function convertUnit(unit){
-      if (unit === 'Fahrenheit') {
-          return baseUnit
-      }
-    else if (unit === 'Celcius') {
-        let newDegrees = Math.round((baseUnit - 32) * (5/9));
-        return newDegrees;
-      }
-      else if (unit === 'Kelvin') {
-        let newDegrees = Math.round((baseUnit - 32) * 5/9 + 273.15)
-        return newDegrees
-        }
+  function convertUnit(unit, base) {
+    if (unit === "Fahrenheit") {
+      return base;
+    } else if (unit === "Celcius") {
+      let newDegrees = Math.round((base - 32) * (5 / 9));
+      return newDegrees;
+    } else if (unit === "Kelvin") {
+      let newDegrees = Math.round(((base - 32) * 5) / 9 + 273.15);
+      return newDegrees;
+    }
   }
-    
+
   function KtoF(k) {
     k = Number(k);
-    if (typeof(k) == "number") {
+    if (typeof k == "number") {
       let f = Math.round((k - 273.15) * (9 / 5) + 32);
       return f;
-    } else console.log("k is not a number",k, typeof(k));
+    } else console.log("k is not a number", k, typeof k);
   }
- 
 
-    function toggleUnit(event) {
-        event.preventDefault()
+  function toggleUnit(event) {
+    event.preventDefault();
 
-        let desiredUnit = unitChoice.current.value
+    let desiredUnit = unitChoice.current.value;
 
-        setTempState(convertUnit(desiredUnit))
-        setFeelState(convertUnit(desiredUnit))
-    }
+    setTempState(convertUnit(desiredUnit, baseUnit));
+    setFeelState(convertUnit(desiredUnit, baseFeel));
+  }
 
-    let units = ['Fahrenheit', 'Celcius', 'Kelvin']
-    const mapUnits = units.map((unit, i) => {
-        return (
-          <option key={i} value={unit}>
-            {unit}
-          </option>
-        );
-      });
+  let units = ["Fahrenheit", "Celcius", "Kelvin"];
+  const mapUnits = units.map((unit, i) => {
+    return (
+      <option key={i} value={unit}>
+        {unit}
+      </option>
+    );
+  });
 
   return (
     <div>
@@ -93,18 +92,18 @@ export default function Form() {
         feel={feelState}
         neighborhood={neighborhoodState}
         description={descriptionState}
-          />
-          <label htmlFor="unitChoice">Choose Units:</label>
-          <br></br>
-          <select
-              onChange={toggleUnit}
-            placeholder="unit dropdown"
-            id="unitChoice"
-              name="unitChoice"
-              ref = {unitChoice}
-          >
-            {mapUnits}
-          </select>
+      />
+      <label htmlFor="unitChoice">Choose Units:</label>
+      <br></br>
+      <select
+        onChange={toggleUnit}
+        placeholder="unit dropdown"
+        id="unitChoice"
+        name="unitChoice"
+        ref={unitChoice}
+      >
+        {mapUnits}
+      </select>
       <section>
         <a className="button">Home Location Weather</a>
         <a className="button">Location Services</a>
